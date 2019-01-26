f529ed1d391010cb4d3e42a0b1ac7cc5060650c3
AAABear <1015445626@qq.com>
2019-01-26 09:25:49 +0800

    init project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 1;
+
+    private Rigidbody2D rigidbody;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+
+        rigidbody.velocity = new Vector2(moveX, 0) * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Cat")
+        {
+            // Show tips
+
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Cat")
+        {
+
+        }
+    }
+}
